d3c0e1e2d55eef0b383d6ba19d75f8d93157acfd
HanJeongSeol <hanjeong94@naver.com>
2023-05-24 16:28:22 +0900

    Section1
    섹션1

--- react-basic/src/App.js
+++ react-basic/src/App.js
@@ -1,4 +1,21 @@
+import { useState } from "react"
+
 function App() {
-    return <div>Hanjeong</div>
+    // [state, 함수]= useState(state 초기값))
+    const [number, setNumber] = useState(1)
+
+    const double = () => {
+        const doubleNumber = number * 2
+        setNumber((prevState) => prevState * 2)
+        setNumber((prevState) => prevState * 2)
+    }
+    return (
+        <>
+            <div>{number}</div>
+            <button className="btn btn-primary" onClick={double}>
+                submit
+            </button>
+        </>
+    )
 }
 export default App
