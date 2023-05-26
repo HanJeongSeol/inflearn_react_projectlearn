1b359e8ff17947ba18d4fb966d7747423354423a
HanJeongSeol <hanjeong94@naver.com>
2023-05-26 17:11:50 +0900

    25_Card_컴포넌트_만들기

--- react-basic/src/pages/ListPage.js
+++ react-basic/src/pages/ListPage.js
@@ -1,5 +1,6 @@
 import axios from "axios"
 import { useState, useEffect } from "react"
+import Card from "../components/Card"
 
 const ListPage = () => {
     const [posts, setPosts] = useState([])
@@ -15,9 +16,10 @@ const ListPage = () => {
     return (
         <div>
             <h1>Blogs</h1>
-            {/* db에서 가져온 post객체의 데이터를 map 함수를 사용해서 가져온다  */}
             {posts.map((post) => {
-                return <div key={post.id}>{post.title}</div>
+                // porps사용 -> 부모 component에서 자식 component에게 데이터를 전달할 수 있다
+                // 전달할 데이터를 속성에 추가한다. title이라는 이름으로 post.title 데이터가 Card 컴포넌트에 전달된다.
+                return <Card key={post.id} title={post.title} />
             })}
         </div>
     )
