8964e993375ec1c1acc72b6cdd3bc174e159c5ca
HanJeongSeol <hanjeong94@naver.com>
2023-05-26 14:23:28 +0900

    20_페이지_추가

--- react-basic/src/App.js
+++ react-basic/src/App.js
@@ -1,6 +1,11 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
-import BlogForm from "./components/BlogForm"
+// BlogFrom 컴포넌트는 CreatePage에서 보여주기 때문에 삭제
 import NavBar from "./components/NavBar"
+// 페이지 컴포넌트 import
+import HomePage from "./pages/HomePage"
+import CreatePage from "./pages/CreatePage"
+import EditPage from "./pages/EditPage"
+import ListPage from "./pages/ListPage"
 
 function App() {
     return (
@@ -8,8 +13,11 @@ function App() {
             <NavBar />
             <div class="container">
                 <Routes>
-                    <Route path="/" element={<div>Home Page</div>}></Route>
-                    <Route path="/blogs" element={<BlogForm />}></Route>
+                    <Route path="/" element={<HomePage />}></Route>
+                    <Route path="/blogs" element={<ListPage />}></Route>
+                    {/* 페이지 추가 */}
+                    <Route path="/blogs/create" element={<CreatePage />}></Route>
+                    <Route path="/blogs/edit" element={<EditPage />}></Route>
                 </Routes>
             </div>
         </Router>
