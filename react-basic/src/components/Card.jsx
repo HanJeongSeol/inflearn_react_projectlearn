7ac95acd524961591618c4e890dfa9bd54d936a8
HanJeongSeol <hanjeong94@naver.com>
2023-05-30 12:50:48 +0900

    28_생성버튼

--- react-basic/src/components/Card.jsx
+++ react-basic/src/components/Card.jsx
@@ -13,20 +13,12 @@ const Card = ({ title, children }) => {
     )
 }
 
-// props로 받아오는 데이터들의 타입을 지정한다
 Card.propTypes = {
-    // title은 string 타입의 데이터를 받아야한다.
-    // 부모(ListPage.js)컴포넌트에서 다른 데이터 타입을 전송시켜서 테스트해보면
-    // 페이지에서는 제대로 보일지로도 콘솔창에서는 에러가 출력된다.
-    // 항상 데이터가 필수로 들어와야 하는 경우 default를 삭제하고 isRequired를 추가하면 된다.
     title: PropTypes.string.isRequired,
     children: PropTypes.element,
 }
 
-//props의 default를 원하는 경우
-// 부모 컴포넌트에서 데이터를 전달하지 않더라도 default 값이 들어간다.
 Card.defaultProps = {
-    // title: "default",
     children: null,
 }
 
--- react-basic/src/pages/ListPage.js
+++ react-basic/src/pages/ListPage.js
@@ -1,6 +1,7 @@
 import axios from "axios"
 import { useState, useEffect } from "react"
 import Card from "../components/Card"
+import { Link } from "react-router-dom"
 
 const ListPage = () => {
     const [posts, setPosts] = useState([])
@@ -15,7 +16,16 @@ const ListPage = () => {
     }, [])
     return (
         <div>
-            <h1>Blogs</h1>
+            {/* css flex 사용 */}
+            <div className="d-flex justify-content-between">
+                <h1>Blogs</h1>
+                {/* /blog/create 페이지로 이동하는 버튼 */}
+                <div>
+                    <Link Link to="/blogs/create" className="btn btn-success">
+                        Create New
+                    </Link>
+                </div>
+            </div>
             {posts.map((post) => {
                 return (
                     <Card key={post.id} title={post.title}>
