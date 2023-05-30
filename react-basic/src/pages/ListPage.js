17b0c41fcf9333324af0ff30c14234ca1f8bdad3
HanJeongSeol <hanjeong94@naver.com>
2023-05-30 13:49:32 +0900

    32_블로그_포스트가_없을경우

--- react-basic/src/pages/ListPage.js
+++ react-basic/src/pages/ListPage.js
@@ -17,12 +17,7 @@ const ListPage = () => {
 
     const deleteBlog = (e, id) => {
         e.stopPropagation()
-        // axios에 delete 요청으로 서버에서 삭제시킨다
-        // db에서는 삭제되어도 아직 posts 객체에는 데이터가 담겨져 있기 때문에 setState를 사용해서 변경사항을 저장해줘야 한다.
         axios.delete(`http://localhost:3001/posts/${id}`).then(() => {
-            // setPosts 메소드로 posts 데이터를 업데이트 한다.
-            // post 객체에서 전달받은 id와 동일하지 않은 객체들만 반환하여 삭제된 데이터는 posts 객체에 포함되지 않도록 한다.
-            // 함수의 반환값에 return 명령어 대신 중괄호를 제외하면 바로 반환시켜준다.
             setPosts((prevPosts) => prevPosts.filter((post) => post.id !== id))
         })
     }
@@ -40,17 +35,24 @@ const ListPage = () => {
                     </Link>
                 </div>
             </div>
-            {posts.map((post) => {
-                return (
-                    <Card key={post.id} title={post.title} onClick={() => navigate("/blogs/edit")}>
-                        <div>
-                            <button className="btn btn-danger btn-sm" onClick={(e) => deleteBlog(e, post.id)}>
-                                Delete
-                            </button>
-                        </div>
-                    </Card>
-                )
-            })}
+            {/* 
+                map 함수를 통해 posts 객체의 데이터를 탐색하고 있다.
+                posts 객체에 데이터가 없을 경우 사용자에게 데이터가 존재하지 않다는 문구를 보여주기 위하여
+                posts 객체의 길이가 1 이상인 경우에만 Card가 보여지도록 한다.
+             */}
+            {posts.length > 0
+                ? posts.map((post) => {
+                      return (
+                          <Card key={post.id} title={post.title} onClick={() => navigate("/blogs/edit")}>
+                              <div>
+                                  <button className="btn btn-danger btn-sm" onClick={(e) => deleteBlog(e, post.id)}>
+                                      Delete
+                                  </button>
+                              </div>
+                          </Card>
+                      )
+                  })
+                : "No blog post found"}
         </div>
     )
 }
