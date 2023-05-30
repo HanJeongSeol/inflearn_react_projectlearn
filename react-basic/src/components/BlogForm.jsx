5654f7ec3a0688a79c32d202040baa59bb64ef32
HanJeongSeol <hanjeong94@naver.com>
2023-05-30 16:30:14 +0900

    43_수정페이지_폼3

--- react-basic/src/components/BlogForm.jsx
+++ react-basic/src/components/BlogForm.jsx
@@ -7,7 +7,11 @@ import PropTypes from "prop-types"
 // bool 타입 데이터를 받는 editing을 매개변수로 설정해서 분기를 설정한다.
 const BlogForm = ({ editing }) => {
     const [title, setTitle] = useState("")
+    // 기존 데이터와 변경 데이터를 비교하기 위해 origanalTitle 생성
+    const [originalTitle, setOriginalTitle] = useState("")
     const [body, setBody] = useState("")
+    const [originalBody, setOriginalBody] = useState("")
+
     // url에서 id 파라미터를 가져온다
     const { id } = useParams()
 
@@ -16,20 +20,34 @@ const BlogForm = ({ editing }) => {
     // Edit으로 들어오는 경우 label, input에 db에 저장되어 있는 데이터가 작성되어 있어야 한다.
     // useEffect를 사용해서 데이터를 불러온다.
     useEffect(() => {
-        axios.get(`http://localhost:3001/posts/${id}`).then((res) => {
-            // title, body값 가져온다
-            setTitle(res.data.title)
-            setBody(res.data.body)
-        })
-    }, [id])
+        // editing인 경우만 실행되도록 한다.
+        if (editing) {
+            axios.get(`http://localhost:3001/posts/${id}`).then((res) => {
+                // title, body값 가져온다
+                setTitle(res.data.title)
+                setOriginalTitle(res.data.title)
+                setBody(res.data.body)
+                setOriginalBody(res.data.body)
+            })
+        }
+    }, [id, editing])
+    // 변경된 데이터가 없을 때 버튼을 비활성 시키기 위한 메소드 둘 중 하나라도 변경된 값이 없으면 비활성화 시킨다.
+    // 일치하지 않을 때 false 반한 -> 데이터가 변경되었다.
+    const isEdited = () => {
+        return title !== originalTitle || body !== originalBody
+    }
 
     // 버튼 클릭 시 edit인 경우 axios.patch로 데이터가 update 되도록 한다.
     const onSubmit = () => {
         if (editing) {
-            axios.patch(`http://localhost:3001/posts/${id}`, {
-                title,
-                body,
-            })
+            axios
+                .patch(`http://localhost:3001/posts/${id}`, {
+                    title,
+                    body,
+                })
+                .then(() => {
+                    navigate(`/blogs/${id}`)
+                })
         } else {
             axios
                 .post("http://localhost:3001/posts", {
@@ -67,8 +85,8 @@ const BlogForm = ({ editing }) => {
                     }}
                 />
             </div>
-
-            <button className="btn btn-primary" onClick={onSubmit}>
+            {/* disabled가 true일 시 버튼이 비활성화 된다. isEdited()가 true인 경우 변경된 데이터가 없는 것이기 때문에 앞에 '!'를 붙인다.*/}
+            <button className="btn btn-primary" onClick={onSubmit} disabled={editing && !isEdited()}>
                 {/* 버튼 이름 변경 */}
                 {editing ? "Edit" : "Post"}
             </button>
