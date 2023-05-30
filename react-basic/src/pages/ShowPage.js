2ea668390106c79153946f0eb8e935eeb76c6f9c
HanJeongSeol <hanjeong94@naver.com>
2023-05-30 15:03:49 +0900

    37_의존성_배열

--- react-basic/src/pages/ShowPage.js
+++ react-basic/src/pages/ShowPage.js
@@ -1,15 +1,17 @@
 import axios from "axios"
 import { useEffect, useState } from "react"
-import { useParams } from "react-router-dom"
+import { useNavigate, useParams } from "react-router-dom"
 import LoadingSpinner from "../components/LoadingSpinner"
 
 const ShowPage = () => {
     const { id } = useParams()
-    // axios로 받아온 데이터 가져오기
+
     const [post, setPost] = useState(null)
-    // 데이터 가져올 때 까지 loading 페이지 보여주기
+
     const [loading, setLoading] = useState(true)
-    // db에 id 값으로 데이터 탐색해서 가져오기
+
+    const navigate = useNavigate()
+
     const getPost = (id) => {
         axios.get(`http://localhost:3001/posts/${id}`).then((res) => {
             setPost(res.data)
@@ -17,11 +19,18 @@ const ShowPage = () => {
         })
     }
 
-    // component가 mount 될 때 한 번 실행된다.
-    // 파라미터로 전달된 id 값을 getPost의 인자값으로 전달하여 db에 id 값이 일치하는 데이터를 post에 저장한다.
+    // useEffect 메소드의 두 번째 매개변수로 들어오는 배열을 의존성 배열이라고 한다.
+    // useEffect 메소드가 배열에 의존해서 작업을 수행한다.
+    // [] : 컴포넌트가 렌더링 될 때 딱 한 번 실행이 된다. 다음 state가 변경되면서 리렌더링 될 때는 실행되지 않는다.
+    // [id] : 컴포넌트가 렌더링 될 때 실행이 되고 id가 변경 될 때마다 useEffect 메솓가 실행된다.
+    // 즉 메소드는 배열에 의존해서 한 번 실행되던지, 재실행되던지 결정된다.
+    // 클릭시 /blogs/3 으로 이동되는 버튼을 생성하고 어떻게 동작하는지 확인해보자. -> url은 변경되었지만 페이지 내용은 변경되지 않는다.
+    // 현재 빈 배열이기 때문에 id 값이 변경되어도 useEffect는 재실행 되지 않아서 url만 변경된다.
+    // [id]로 id값이 변경될 때 useEffect가 실행되도록 해보면 url, 데이터 모두 변경되는 것을 확인할 수 있다.
     useEffect(() => {
+        console.log("hello")
         getPost(id)
-    }, [])
+    }, [id])
 
     if (loading) {
         return <LoadingSpinner />
@@ -30,6 +39,13 @@ const ShowPage = () => {
         <div>
             <h1>{post.title}</h1>
             <p>{post.body}</p>
+            <button
+                onClick={() => {
+                    navigate("/blogs/3")
+                }}
+            >
+                Click
+            </button>
         </div>
     )
 }
