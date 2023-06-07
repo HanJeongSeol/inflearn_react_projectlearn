5eb80a14399ce83102d97efe189526358516b245
HanJeongSeol <hanjeong94@naver.com>
2023-06-07 13:40:08 +0900

    50_블로그생성후_admin이동

--- react-basic/src/components/Card.jsx
+++ react-basic/src/components/Card.jsx
@@ -3,9 +3,7 @@ import PropTypes from "prop-types"
 const Card = ({ title, onClick, children }) => {
     return (
         <div className="card mb-3 cursor-pointer" onClick={onClick}>
-            {/* padding y 축 변경, 글자 가운데 정렬*/}
             <div className="card-body py-2 d-flex align-items-center">
-                {/* css flex 수정, , delete 버튼 우측 고정  */}
                 <div className="flex-grow-1">{title}</div>
                 {children && <div>{children}</div>}
             </div>
