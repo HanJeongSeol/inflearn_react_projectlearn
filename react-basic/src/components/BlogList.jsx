64274b5c52b8248e45e040210cc6858b313407d9
HanJeongSeol <hanjeong94@naver.com>
2023-06-07 15:00:52 +0900

    52_pagination_3

--- react-basic/src/components/BlogList.jsx
+++ react-basic/src/components/BlogList.jsx
@@ -16,10 +16,20 @@ const BlogList = ({ isAdmin }) => {
     const getPosts = (page = 1) => {
         // json-server로 페이징 처리를 하려면 쿼리스트링으로 _page=? & _limit=? 값을 제공함으로서 구현한다
         // _sort=? : 정렬기준점, _order=? : 오름차순,내림차순(DESC) 설정
-        axios.get(`http://localhost:3001/posts?_page=${page}&_limit=5&_sort=id&_order=desc`).then((res) => {
-            setPosts(res.data)
-            setLoading(false)
-        })
+        // pagination3 -> 쿼리스트링 부분을 params 객체로 전달 할 수 있다.
+        axios
+            .get(`http://localhost:3001/posts`, {
+                params: {
+                    _page: page,
+                    _limit: 5,
+                    _sort: "id",
+                    _order: "desc",
+                },
+            })
+            .then((res) => {
+                setPosts(res.data)
+                setLoading(false)
+            })
     }
     const deleteBlog = (e, id) => {
         e.stopPropagation()
