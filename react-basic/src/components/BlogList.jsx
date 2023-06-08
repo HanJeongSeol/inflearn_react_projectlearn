b5967fe11921f32951e7b4fcbb61c12c6f1ad76c
HanJeongSeol <hanjeong94@naver.com>
2023-06-08 12:32:29 +0900

    60_usecallback

--- react-basic/src/components/BlogList.jsx
+++ react-basic/src/components/BlogList.jsx
@@ -1,5 +1,5 @@
 import axios from "axios"
-import { useState, useEffect } from "react"
+import { useState, useEffect, useCallback } from "react"
 import Card from "../components/Card"
 import LoadingSpinner from "../components/LoadingSpinner"
 import { useNavigate } from "react-router-dom"
@@ -9,73 +9,54 @@ import { useLocation } from "react-router-dom"
 
 const BlogList = ({ isAdmin }) => {
     const navigate = useNavigate()
-    // useLocation을 사용해서 url의 쿼리스트링 부분을 가져올 수 있다 -> ?page=7
     const location = useLocation()
-    // URLSearchParams를 사용해서 URL 파라미터 값을 key-value로 전달한다.
     const params = new URLSearchParams(location.search)
-    // params.get('key') -> key에 해당되는 value를 가져온다
     const pageParam = params.get("page")
     const [posts, setPosts] = useState([])
-
     const [loading, setLoading] = useState(true)
-
-    // 처음 currentPage를 1로 설정, pagination 컴포넌트에서
     const [currentPage, setCurrentPage] = useState(1)
-    // 총 Posts 수 -> res.header의 x-total-count 값으로 확인
     const [numberOfPosts, setNumberOfPosts] = useState(0)
-    // 총 페이지 수
     const [numberOfPages, setNumberOfPages] = useState(0)
-    // 한 페이지에서 보여줄 post 수
     const limit = 1
 
-    // 전체 posts 수가 변경 될 때 마다 실행(numberOfPosts) -> 총 페이지 수 계산
     useEffect(() => {
         setNumberOfPages(Math.ceil(numberOfPosts / limit))
     }, [numberOfPosts])
 
-    // navigate를 사용해서 클릭해서 이동한 페이지가 기록되도록 한다.
-    // 뒤로가기 버튼 클릭 시 기록된 이전 페이지로 이동이 가능하다. (url 변경디는거 확인)
-    // 이동 후 다시 getPosts 실행하여 값을 업데이트 해야한다.
-    // location.pathname => blog에서 페이지 클릭시 똑같이 blog 페이지에서 이동되도록 pathoname를 가져왔다.
     const onClickPageButton = (page) => {
         navigate(`${location.pathname}?page=${page}`)
         getPosts(page)
     }
+    // 함수는 렌더링 될 때마다 새로 생성된다.
+    // useCallback 추가
+    // 첫번째로 함수 인자, 두번째로 디펜던시를 받는다. 함수를 기억해서 컴포넌트가 리렌더링 되어도 새로운 함수를 생성하지 않고 기억된 함수를 그대로 사용한다.
+    const getPosts = useCallback(
+        (page = 1) => {
+            let params = {
+                _page: page,
+                _limit: limit,
+                _sort: "id",
+                _order: "desc",
+            }
 
-    // prop를 받아와서 보여줄 page를 지정, default 1페이지로 설정
-    // page의 인자 값으로 pagination 컴포넌트에서 pageNumber을 전달받는다.
-    // setCurrentPage를 실행시켜서 currentPage 값을 변경시킨다 -> 페이지 클릭 시 파란색 강조 부분도 변경되려면 리렌더링이 되어야 한다.
-    const getPosts = (page = 1) => {
-        // json-server로 페이징 처리를 하려면 쿼리스트링으로 _page=? & _limit=? 값을 제공함으로서 구현한다
-        // _sort=? : 정렬기준점, _order=? : 오름차순(ASC),내림차순(DESC) 설정
-        // pagination3 -> 쿼리스트링 부분을 params 객체로 전달 할 수 있다.
-        // pagination4 -> publish가 true인 게시글만 가져온다.
-        // admin에서는 true,false 상관없이 전부 보여줘야하니 params를 외부 변수로 빼낸 후 publish에 대해서만 조건문 사용
+            if (!isAdmin) {
+                params = { ...params, publish: true }
+            }
 
-        let params = {
-            _page: page,
-            _limit: limit,
-            _sort: "id",
-            _order: "desc",
-        }
-        // isAdmin이 아닌 경우 기존 params 객체에 publish:true 값을 추가
-        if (!isAdmin) {
-            params = { ...params, publish: true }
-        }
+            axios
+                .get(`http://localhost:3001/posts`, {
+                    params: params,
+                })
 
-        axios
-            .get(`http://localhost:3001/posts`, {
-                params: params,
-            })
-            // res 객체의 heder를 살펴보면 x-total-count 값을 확인할 수 있다.
-            // 해당 값은 요청 후 전달받은 데이터의 총 갯수이다.
-            // 페이지별로 5개씩 보여주기로 했으니 x-total-count / 5 의 결과를 올림 처리 하면 필요한 페이지 갯수를 정할 수 있다.
-            .then((res) => {
-                setNumberOfPosts(res.headers["x-total-count"])
-                setPosts(res.data)
-                setLoading(false)
-            })
-    }
+                .then((res) => {
+                    setNumberOfPosts(res.headers["x-total-count"])
+                    setPosts(res.data)
+                    setLoading(false)
+                })
+        },
+        // useCallback에서 사용하는 디펜던시
+        [isAdmin]
+    )
 
     const deleteBlog = (e, id) => {
         e.stopPropagation()
@@ -83,12 +64,13 @@ const BlogList = ({ isAdmin }) => {
             setPosts((prevPosts) => prevPosts.filter((post) => post.id !== id))
         })
     }
-
-    // 기존 useEffect는 삭제, pageParam 변경이 일어날 시 리렌더링 되도록 하나.
-    // 이 때 getPosts의 인자 값은 number 타입이 필요. pageParam은 string이기 때문에 parseInt사용
+    // getPosts를 dependenct에 추가해야한다.
+    // 이 때 setCurrentPage 값이 변경되면서 리렌더링 되고 새로운 getPosts 함수가 생성된다
+    // getPosts를 호출하면 또 다른 getPosts 함수가 생성된다.
+    // 리렌더링 되면서 다시 useEffect()가 실행 -> 무한반복 -> getPosts 함수가 무한 생성 -> 메모리 터짐
+    // 1. getPosts 함수를 useEffect() 내부에 넣기 -> 디펜던시 삭제 -> isAdmin 디펜던시 추가
+    // 2. useCallback 훅 사용
     useEffect(() => {
-        // getPosts 내부의 setCurrentPage도 옮겨준다.
-        // 파라미터가 존재하지 않으면(/admin으로 접속할 시 뒤에 쿼리스트링이 존재하지 않는다.) 1페이지가 보여지도록 한다.
         setCurrentPage(parseInt(pageParam) || 1)
         getPosts(parseInt(pageParam) || 1)
     }, [pageParam])
@@ -100,10 +82,6 @@ const BlogList = ({ isAdmin }) => {
     if (posts.length === 0) {
         return <div>No post found blog</div>
     }
-    // 기존 렌더링 부분을 함수로 변경
-    // filter을 통해서 publish가 true인 게시글만 보여지게 되는데
-    // pagination 처리를 할 때 처음 5개에서 true가 없다면 하나의 게시글도 보여지지 않게 된다.
-    // filter를 삭제하고 서버에서 true인 게시글만 조회한 후 프론트로 전달하도록 하자.
     const renderBlogList = () => {
         return posts.map((post) => {
             return (
@@ -119,18 +97,9 @@ const BlogList = ({ isAdmin }) => {
             )
         })
     }
-    // 리스트 렌더링, 페이징 부분
     return (
         <div>
             {renderBlogList()}
-            {/* currentPage 값을 넘겨서 현재 페이지를 알려준다. 
-                numberOfPages가 1보다 작은 경우는 한 페이지에 모든 요소들이 보여지는 경우다.
-                따라서 numberOfPages가 1보다 클 때 Pagination이 보여지도록 한다.
-            */}
-
-            {/* 
-                navigate가 적용된 onClickPageButton 함수 사용
-            */}
             {numberOfPages > 1 && (
                 <Pagination currentPage={currentPage} numberOfPages={numberOfPages} onClick={onClickPageButton} />
             )}
--- react-basic/src/components/pagination.jsx
+++ react-basic/src/components/pagination.jsx
@@ -1,41 +1,21 @@
 import propTypes from "prop-types"
 
-// BlogList 컴포넌트에서 사용될 때 currentPage를 넘기지 않아도 default로 1을 설정했기 때문에 적용되는거 확인
-// 삼항연산자 사용해서 currentPage 값에 따라서 active 들어가는 위치 변경
-// limit : 현재 페이지 기준으로 보여줄 페이지 갯수, 1페이지 클릭 -> 1~5페이지만 보인다
 const Pagination = ({ currentPage, numberOfPages, onClick, limit }) => {
-    // 현재 set -> 1~5 : 1set, 6~10 : 2set
-    // limit=5 즉 한번에 5개의 페이지 목록만 보여지도록 한다.
-    // 현재 페이지 / limit의 값을 구한 후 올림을 하여 현재 몇 set인지 구한다.
     const currentSet = Math.ceil(currentPage / limit)
-
-    // 마지막 set 값,
     const lastSet = Math.ceil(numberOfPages / limit)
-
-    // limit* currentSet = 5, 10, 15...
-    // 첫 페이지는 1, 6, 11, 16 ... 따라서 +1
-    // 1set는 1~5페이지, 따라서 currnetSet - 1
     const startPage = limit * (currentSet - 1) + 1
-    // numberOfPagesForSet -> 현재 페이지에서 보여질 post 수
-    // 기본 limit-> 5개로 설정하고 마지막 set에서만 총 페이지 수를 limit으로 나눈 나머지 값으로 설정한다.
-    // 즉 마지막 페이지 set에서 남은 post를 모드 보여주게 된다.
     const numberOfPagesForSet = currentSet === lastSet ? numberOfPages % limit : limit
 
     return (
         <nav aria-label="Page navigation example">
             <ul className="pagination justify-content-center">
-                {/* currentSet이 1인경우 prev가 보여질 필요 없다. */}
                 <li className="page-item">
                     {currentSet !== 1 && (
-                        // startPage-limit = 이전 pageSet 값
                         <div className="page-link cursor-pointer" onClick={() => onClick(startPage - limit)}>
                             Previous
                         </div>
                     )}
                 </li>
-                {/* 보여질 페이지 갯수를 전달받은 후 Array 객체의 fill 함수로 numberOfPages 크기의 배열을 생성
-                    fill 함수로 배열을 1로 채운 후, map 함수로 1+index 값을 넣어준다. index는 0부터 시작하기 때문에 배열에 1~5가 저장된다
-                    다시 map 함수를 사용해서 중첩되었던 코드를 간략화 시킨다. */}
 
                 {Array(numberOfPagesForSet)
                     .fill(startPage)
@@ -45,8 +25,6 @@ const Pagination = ({ currentPage, numberOfPages, onClick, limit }) => {
                             <li key={pageNumber} className={`page-item ${currentPage === pageNumber ? "active" : ""}`}>
                                 <div
                                     className="page-link cursor-pointer"
-                                    // BlogList의 getPosts 함수의 매개변수로 page값을 갖는다.
-                                    // onClick 실행 시 클릭한 페이지 값을 넘겨주도록 한다.
                                     onClick={() => {
                                         onClick(pageNumber)
                                     }}
@@ -56,11 +34,9 @@ const Pagination = ({ currentPage, numberOfPages, onClick, limit }) => {
                             </li>
                         )
                     })}
-                {/* currentSet과 lastSet이 같으면 마지막 페이지임 */}
+
                 {currentSet !== lastSet && (
                     <li className="page-item">
-                        {/* onClick 메소드의 인자 값으로 햔재 페이지 + limit 값을 전달해서 다음 set으로 이동시킬 수 있다.
-                        startPage는 사용자가 클릭한 페이지가 아닌 set의 시작 page, 1, 6, 11*/}
                         <div className="page-link cursor-pointer" onClick={() => onClick(startPage + limit)}>
                             Next
                         </div>
@@ -73,7 +49,6 @@ const Pagination = ({ currentPage, numberOfPages, onClick, limit }) => {
 
 Pagination.propTypes = {
     currentPage: propTypes.number,
-    // 몇개의 페이지를 보여줄지 정해주는 prop
     numberOfPages: propTypes.number,
     onClick: propTypes.func.isRequired,
     limit: propTypes.number,
